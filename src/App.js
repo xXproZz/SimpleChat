0109062f67578c393e1653da0d61be2e45f6f88a
xXproZz <139834779+xXproZz@users.noreply.github.com>
2023-07-21 22:11:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { Box } from '@mui/material';
 import './App.css';
 import Chat from './Components/Chat';
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Box, Grid } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      
-     <h3><ChatBubbleIcon  className='App-Icon'/>SimpleChat</h3>
+
+    <Box sx={{
+      p: 1,
+      backgroundColor: "grey.200",
+    }}> 
+    <Grid 
+    container
+    direction="row"
+    justifyContent="space-between"
+    alignItems="center" >
+      <h3><ChatBubbleIcon  className='App-Icon'/>SimpleChat</h3>
+      <SettingsIcon  className='App-IconSettings'
+      />
+    </Grid>
+    
+    </Box>
      <Chat />
     </div>
   );
